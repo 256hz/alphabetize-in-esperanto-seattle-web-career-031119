7673a9a082c00763e960f1b9ace0068f299f7987
Abe D <iaryiou111@gmail.com>
2019-03-07 23:50:19 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -15,12 +15,13 @@ def alphabetize(arr, e)
     temp_array = output_array
     output_array = []
     counter += 1
-    puts "counter: #{counter}\ntemp_array:\n#{temp_array}\n"
     sorted = true
+    puts "counter: #{counter}\ntemp_array:\n#{temp_array}\n"
     while temp_array.length > 1
       a = get_num(temp_array[0][0], e)
       b = get_num(temp_array[1][0], e)
       #binding.pry
+      puts 
       case b <=> a
       when -1
         output_array << temp_array.delete_at(1)
@@ -28,10 +29,10 @@ def alphabetize(arr, e)
         puts "'#{temp_array[1]}' comes before '#{temp_array[0]}'"
       when 0
         puts "#{temp_array[0]} same as #{temp_array[1]}"
-        output_array << temp_array.delete_at(0)
+        output_array << temp_array.shift
       when 1
         puts "'#{temp_array[0]}' comes before '#{temp_array[1]}'"
-        output_array << temp_array.delete_at(0)
+        output_array << temp_array.shift
       end
       if temp_array.length == 1
         puts "loop over"
