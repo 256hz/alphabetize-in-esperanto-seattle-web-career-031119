86cea44b1897bd5ce54238e8892c5e1f490a4efb
Abe D <iaryiou111@gmail.com>
2019-03-07 23:54:57 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -16,7 +16,7 @@ def alphabetize(arr, e)
     output_array = []
     counter += 1
     sorted = true
-    puts "counter: #{counter}\ntemp_array:\n#{temp_array}\n"
+#    puts "counter: #{counter}\ntemp_array:\n#{temp_array}\n"
     while temp_array.length > 1
       a = get_num(temp_array[0][0], e)
       b = get_num(temp_array[1][0], e)
@@ -24,15 +24,15 @@ def alphabetize(arr, e)
       puts 
       case b <=> a
       when -1
-        puts "'#{temp_array[1]}' comes before '#{temp_array[0]}'"
-        puts "false sorted triggered"
+#        puts "'#{temp_array[1]}' comes before '#{temp_array[0]}'"
+#        puts "false sorted triggered"
         output_array << temp_array.delete_at(1)
         sorted = false
       when 0
-        puts "#{temp_array[0]} same as #{temp_array[1]}"
+#        puts "#{temp_array[0]} same as #{temp_array[1]}"
         output_array << temp_array.shift
       when 1
-        puts "'#{temp_array[0]}' comes before '#{temp_array[1]}'"
+#        puts "'#{temp_array[0]}' comes before '#{temp_array[1]}'"
         output_array << temp_array.shift
       end
       if temp_array.length == 1
@@ -40,8 +40,8 @@ def alphabetize(arr, e)
       end
     end
     output_array << temp_array.delete_at(0)
-    puts "\noutput at end of loop #{counter}:\n#{output_array}\n\n"
-    puts "sorted = #{sorted}"
+#    puts "\noutput at end of loop #{counter}:\n#{output_array}\n\n"
+#    puts "sorted = #{sorted}"
     counter += 1
     #binding.pry
     if sorted == true
